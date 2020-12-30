1b4d5830239eb706a4b3b8da8358a4c972002c53
Lúcás Meier <cronokirby@gmail.com>
2020-12-30 19:02:09 +0100

    Add link to supported functions in input

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import katex from "katex";
 import Header from "./Header";
 import Logo from "./Logo";
+import Help from "./Help";
 
 interface KatexProps {
   text: string;
@@ -26,7 +27,15 @@ function Editor() {
   return (
     <div className="flex flex-col items-center space-y-6 mt-8">
       <div className="px-4 py-2 bg-white rounded shadow-md w-11/12 md:w-10/12 lg:w-5/12">
-        <h2 className="mb-4 font-bold text-gray-600">Math Input</h2>
+        <div className="mb-4 flex items-center justify-between">
+          <h2 className="font-bold text-gray-800">Math Input</h2>
+          <a
+            href="https://katex.org/docs/supported.html"
+            className="transition-colors duration-300 text-gray-800 hover:text-gray-600 p-1 rounded"
+          >
+            <Help />
+          </a>
+        </div>
         <textarea
           className="h-64 sm:h-32 px-2 py-1 md:h-64 font-mono shadow-inner rounded bg-gray-50 w-full"
           spellCheck={false}
