e00c018bc58514d2bb7be84f3df1286d0bd0396a
Lúcás Meier <cronokirby@gmail.com>
2020-12-30 18:25:41 +0100

    Basic katex rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import katex from "katex";
+
+interface KatexProps {
+  text: string;
+}
+
+function Katex({ text }: KatexProps) {
+  const ref = React.useRef<HTMLDivElement | null>(null);
+  React.useEffect(() => {
+    if (ref.current) {
+      katex.render(text, ref.current, {
+        throwOnError: false,
+        displayMode: true,
+      });
+    }
+  }, [ref, text]);
+
+  return <div ref={ref}></div>;
+}
 
 function Editor() {
-  const [text, setText] = React.useState("\\sum_{i = 1}^{infty}");
+  const [text, setText] = React.useState("\\sum_{i = 1}^{\\infty}");
   return (
     <div className="w-full h-screen">
       <textarea
@@ -10,7 +29,7 @@ function Editor() {
         value={text}
         onChange={(e) => setText(e.target.value)}
       ></textarea>
-      <div className="p-2 h-full text-lg">{text}</div>
+      <Katex text={text} />
     </div>
   );
 }
