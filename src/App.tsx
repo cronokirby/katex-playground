f99aae2388e839c7cb8141b63360cf8ca97efe91
Lúcás Meier <cronokirby@gmail.com>
2020-12-30 17:09:54 +0100

    Add Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
 
 function App() {
   return (
---
+++ src/d.ts
@@ -0,0 +1,2 @@
+// Without this declaration, Typescript will complain when we import static files
+declare module "*.svg";
